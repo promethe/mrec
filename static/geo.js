26d515d31c3dbb2a483d73aea55b79dea6eaabda
bogo <bogo.giertler@gmail.com>
2011-03-16 12:45:38 +0000

    ~70% of the new changes for the final milestone.

--- static/geo.js
+++ static/geo.js
@@ -1,30 +1,47 @@
-if (navigator.geolocation) {
-  navigator.geolocation.getCurrentPosition(success, error);
-} else {
-  error('not supported');
+var initialLocation;
+var map;
+
+function initialize() {
+    var myOptions = {
+      zoom: 16,
+      center: initialLocation,
+      disableDefaultUI: true,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+
+    map = new google.maps.Map(document.getElementById("map_canvas"),
+        myOptions);
+
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(success, error);
+    } else {
+        error('not supported');
+    }
 }
 
 function success(position) {
-    alert("Zecholokowany!");
-    // $('#status').removeClass('fail');
+    // add success notification
+    initialLocation = new google.maps.LatLng(position.coords.latitude,position.coords.longitude);
+    map.setCenter(initialLocation);
+
     $('input[name=longitude]').val(position.coords.longitude);
     $('input[name=latitude]').val(position.coords.latitude);
-    
-    // 
-    // if (jQuery) {  
-    //    alert("Hello!");
-    // } else {
-    //     alert("Not loaded!");
-    // }
-    // 
-    // $('divstatus').addClass('success');
-    // 
-    // document.querySelector('#longitude').value = position.coords.longitude
-    // document.querySelector('#latitude').value = position.coords.latitude
-
 }
 
 function error(msg) {
-  alert('fail!');
+  alert('Couldn\'t localize the device using Webkit!');
   // console.log(arguments);
+}    
+
+function detectBrowser() {
+  var useragent = navigator.userAgent;
+  var mapdiv = document.getElementById("map_canvas");
+    
+  if (useragent.indexOf('iPhone') != -1 || useragent.indexOf('Android') != -1 ) {
+    mapdiv.style.width = '100%';
+    mapdiv.style.height = '100%';
+  } else {
+    mapdiv.style.width = '600px';
+    mapdiv.style.height = '800px';
+  }
 }
